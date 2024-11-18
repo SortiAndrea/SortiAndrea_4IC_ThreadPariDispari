06c6b10cf4c75a5c371d03a126805b86a4e2bba0
andre <andre@PCANDREASORTI>
2024-11-18 18:31:09 +0100

    Aggiungere i file di progetto.

---
+++ SortiAndrea_4IC_ThreadPariDispari/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SortiAndrea_4IC_ThreadPariDispari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbThreads = new System.Windows.Forms.ListBox();
+            this.btnThread_Pari = new System.Windows.Forms.Button();
+            this.btnThread_Dispari = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbThreads
+            // 
+            this.lbThreads.FormattingEnabled = true;
+            this.lbThreads.ItemHeight = 16;
+            this.lbThreads.Location = new System.Drawing.Point(12, 12);
+            this.lbThreads.Name = "lbThreads";
+            this.lbThreads.Size = new System.Drawing.Size(776, 260);
+            this.lbThreads.TabIndex = 0;
+            // 
+            // btnThread_Pari
+            // 
+            this.btnThread_Pari.Location = new System.Drawing.Point(12, 289);
+            this.btnThread_Pari.Name = "btnThread_Pari";
+            this.btnThread_Pari.Size = new System.Drawing.Size(369, 149);
+            this.btnThread_Pari.TabIndex = 1;
+            this.btnThread_Pari.Text = "Avvia thread 1 (pari)";
+            this.btnThread_Pari.UseVisualStyleBackColor = true;
+            this.btnThread_Pari.Click += new System.EventHandler(this.btnThread_Pari_Click);
+            // 
+            // btnThread_Dispari
+            // 
+            this.btnThread_Dispari.Location = new System.Drawing.Point(419, 289);
+            this.btnThread_Dispari.Name = "btnThread_Dispari";
+            this.btnThread_Dispari.Size = new System.Drawing.Size(369, 149);
+            this.btnThread_Dispari.TabIndex = 2;
+            this.btnThread_Dispari.Text = "Avvia thread 2 (dispari)";
+            this.btnThread_Dispari.UseVisualStyleBackColor = true;
+            this.btnThread_Dispari.Click += new System.EventHandler(this.btnThread_Dispari_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThread_Dispari);
+            this.Controls.Add(this.btnThread_Pari);
+            this.Controls.Add(this.lbThreads);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbThreads;
+        private System.Windows.Forms.Button btnThread_Pari;
+        private System.Windows.Forms.Button btnThread_Dispari;
+    }
+}
+
---
+++ SortiAndrea_4IC_ThreadPariDispari/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortiAndrea_4IC_ThreadPariDispari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        int[] numeriPari = new int[100];
+
+        int[] numeriDispari = new int[100];
+
+        static int turno = 0;
+
+        static int numeroCorrentePari = -2; // se voglio che parta da 0 metto -2, se voglio che parta da 2 metto 0
+
+        static int numeroCorrenteDispari = -1; // se voglio che parta da 1 metto -1, se voglio che parta da 3 metto 1
+
+        private void btnThread_Pari_Click(object sender, EventArgs e)
+        {
+            Thread pariThread = new Thread(new ThreadStart(GeneraPari));
+            pariThread.Start();
+        }
+
+        private void btnThread_Dispari_Click(object sender, EventArgs e)
+        {
+            Thread dispariThread = new Thread(new ThreadStart(GeneraDispari));
+            dispariThread.Start();
+        }
+
+        public void GeneraPari()
+        {
+            while (true)
+            {
+                while (turno != 0)
+                    Thread.Sleep(100);
+
+                lock (numeriPari)
+                {
+                    numeroCorrentePari += 2;
+                    turno = 1;
+
+                    //Invoke(new Action(() =>
+                    //{
+                    //    lbThreads.Items.Add($"Numero pari: {numeroCorrentePari}");
+                    //}));
+
+                    lbThreads.Items.Add($"Numero pari: {numeroCorrentePari}");
+                }
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        public void GeneraDispari()
+        {
+            while (true)
+            {
+                while (turno != 1)
+                    Thread.Sleep(100);
+
+                lock (numeriDispari)
+                {
+                    numeroCorrenteDispari += 2;
+                    turno = 0;  // Passa il turno ai numeri pari
+
+                    //Invoke(new Action(() =>
+                    //{
+                    //    lbThreads.Items.Add($"Numero dispari: {numeroCorrenteDispari}");
+                    //}));
+
+                    lbThreads.Items.Add($"Numero dispari: {numeroCorrenteDispari}");
+                }
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
